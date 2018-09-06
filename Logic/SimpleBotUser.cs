14e2811a6b55d9bd0e1b688bd9f2d26c59aa49e0
Rodrigo Belmonte de Oliveira <rod.b.oliveira@gmail.com>
2018-09-05 22:06:01 -0300

    Start MongoDB

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
 
 namespace SimpleBot
 {
     public class SimpleBotUser
     {
+
         public static string Reply(Message message)
         {
+            string conn = "mongodb://localhost:27017";
+
+            var client = new MongoClient(conn);
+
+
+            var doc = new BsonDocument
+            {
+                {"id", message.Id },
+                {"texto", message.Text},
+                {"botName", "17" },
+                {message.User, "Rodrigo"  }
+            };
+
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<BsonDocument>("tabela01");
+
+            col.InsertOne(doc);
+
             return $"{message.User} disse '{message.Text}'";
+
         }
 
         public static UserProfile GetProfile(string id)
